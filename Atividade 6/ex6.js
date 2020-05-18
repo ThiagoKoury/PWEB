05a8fbfdd8377f56c02f90711190ffc7db0cb32b
Thiago Koury <thiagokoury25@gmail.com>
2020-05-18 12:20:20 -0300

    first commit

---
+++ Atividade 6/ex6.js
@@ -0,0 +1,14 @@
+var aluno = {
+    nome: "Thiago",
+    ra: "0030481811034"
+
+};
+
+var aluno2 = new Object();
+aluno.nome = "Thiago";
+aluno.ra = "0030481811034";
+
+var aluno3 = {};
+aluno.nome = "Thiago";
+aluno.ra = "0030481811034";
+
